fb673ed364c4f21f844425989cd7b61fe4e8c303
GHeadlines User <user@gheadlines.local>
2025-12-13 19:52:15 +0530

    Fix RSS validation and compliance

--- handlers/rss.go
+++ handlers/rss.go
@@ -9,10 +9,13 @@ import (
 )
 
 type RSS struct {
-	XMLName   xml.Name `xml:"rss"`
-	Version   string   `xml:"version,attr"`
-	XmlnsAtom string   `xml:"xmlns:atom,attr"`
-	Channel   Channel  `xml:"channel"`
+	XMLName      xml.Name `xml:"rss"`
+	Version      string   `xml:"version,attr"`
+	XmlnsAtom    string   `xml:"xmlns:atom,attr"`
+	XmlnsMedia   string   `xml:"xmlns:media,attr"`
+	XmlnsDc      string   `xml:"xmlns:dc,attr"`
+	XmlnsContent string   `xml:"xmlns:content,attr"`
+	Channel      Channel  `xml:"channel"`
 }
 
 type Channel struct {
@@ -32,17 +35,31 @@ type AtomLink struct {
 }
 
 type Item struct {
-	Title       string     `xml:"title"`
-	Link        string     `xml:"link"`
-	Description string     `xml:"description"`
-	PubDate     string     `xml:"pubDate"`
-	GUID        string     `xml:"guid"`
-	Enclosure   *Enclosure `xml:"enclosure,omitempty"`
+	Title          string        `xml:"title"`
+	Link           string        `xml:"link"`
+	Description    string        `xml:"description"`
+	PubDate        string        `xml:"pubDate"`
+	GUID           string        `xml:"guid"`
+	Enclosure      *Enclosure    `xml:"enclosure,omitempty"`
+	Creator        string        `xml:"dc:creator,omitempty"`
+	Category       string        `xml:"category,omitempty"`
+	ContentEncoded *CDATA        `xml:"content:encoded,omitempty"`
+	MediaContent   *MediaContent `xml:"media:content,omitempty"`
 }
 
 type Enclosure struct {
 	URL  string `xml:"url,attr"`
 	Type string `xml:"type,attr"`
+}
+
+type MediaContent struct {
+	URL    string `xml:"url,attr"`
+	Type   string `xml:"type,attr"`
+	Medium string `xml:"medium,attr"`
+}
+
+type CDATA struct {
+	Value string `xml:",cdata"`
 }
 
 // RSSHandler generates and serves RSS feed
@@ -56,8 +73,11 @@ func RSSHandler(dbClient *db.Client, siteURL string, siteName string) http.Handl
 		}
 
 		rss := RSS{
-			Version:   "2.0",
-			XmlnsAtom: "http://www.w3.org/2005/Atom",
+			Version:      "2.0",
+			XmlnsAtom:    "http://www.w3.org/2005/Atom",
+			XmlnsMedia:   "http://search.yahoo.com/mrss/",
+			XmlnsDc:      "http://purl.org/dc/elements/1.1/",
+			XmlnsContent: "http://purl.org/rss/1.0/modules/content/",
 			Channel: Channel{
 				Title:         siteName,
 				Link:          siteURL,
@@ -85,13 +105,26 @@ func RSSHandler(dbClient *db.Client, siteURL string, siteName string) http.Handl
 				Description: a.Excerpt,
 				PubDate:     a.CreatedAt.Format(time.RFC1123),
 				GUID:        link,
+				Creator:     a.Author,
+				Category:    a.Category,
 			}
 
-			// Add image enclosure if available
+			if a.Content != "" {
+				item.ContentEncoded = &CDATA{Value: a.Content}
+			}
+
+			// Add image enclosure and media:content for better compatibility
 			if a.ImageURL != "" {
+				// Standard Enclosure (Podcasts/Legacy)
 				item.Enclosure = &Enclosure{
 					URL:  a.ImageURL,
-					Type: "image/jpeg", // Assuming JPEG for simplicity, or strict check
+					Type: "image/jpeg",
+				}
+				// Media RSS (Google News/Discovery)
+				item.MediaContent = &MediaContent{
+					URL:    a.ImageURL,
+					Type:   "image/jpeg",
+					Medium: "image",
 				}
 			}
 
