454ce9bbd6ae3fbe3a574a43ccd6b475e7bead86
GHeadlines User <user@gheadlines.local>
2025-12-13 19:57:34 +0530

    Reshape RSS to match Times of India structure

--- handlers/rss.go
+++ handlers/rss.go
@@ -12,20 +12,29 @@ type RSS struct {
 	XMLName      xml.Name `xml:"rss"`
 	Version      string   `xml:"version,attr"`
 	XmlnsAtom    string   `xml:"xmlns:atom,attr"`
-	XmlnsMedia   string   `xml:"xmlns:media,attr"`
 	XmlnsDc      string   `xml:"xmlns:dc,attr"`
-	XmlnsContent string   `xml:"xmlns:content,attr"`
+	XmlnsMedia   string   `xml:"xmlns:media,attr"`   // Keeping for Google Discover support
+	XmlnsContent string   `xml:"xmlns:content,attr"` // Keeping for full content support
 	Channel      Channel  `xml:"channel"`
 }
 
 type Channel struct {
-	Title         string    `xml:"title"`
-	Link          string    `xml:"link"`
-	Description   string    `xml:"description"`
-	Language      string    `xml:"language"`
-	LastBuildDate string    `xml:"lastBuildDate"`
-	AtomLink      *AtomLink `xml:"atom:link,omitempty"`
-	Item          []Item    `xml:"item"`
+	AtomLink      *AtomLink     `xml:"atom:link,omitempty"`
+	Title         string        `xml:"title"`
+	Link          string        `xml:"link"`
+	Description   string        `xml:"description"`
+	Language      string        `xml:"language"`
+	Copyright     string        `xml:"copyright"`
+	Docs          string        `xml:"docs"`
+	Image         *ChannelImage `xml:"image,omitempty"`
+	LastBuildDate string        `xml:"lastBuildDate"`
+	Item          []Item        `xml:"item"`
+}
+
+type ChannelImage struct {
+	Title string `xml:"title"`
+	Link  string `xml:"link"`
+	URL   string `xml:"url"`
 }
 
 type AtomLink struct {
@@ -35,21 +44,21 @@ type AtomLink struct {
 }
 
 type Item struct {
-	Title          string        `xml:"title"`
-	Link           string        `xml:"link"`
-	Description    string        `xml:"description"`
+	Title          CDATA         `xml:"title"`
+	Description    CDATA         `xml:"description"`
+	Link           CDATA         `xml:"link"`
+	GUID           CDATA         `xml:"guid"`
 	PubDate        string        `xml:"pubDate"`
-	GUID           string        `xml:"guid"`
-	Enclosure      *Enclosure    `xml:"enclosure,omitempty"`
 	Creator        string        `xml:"dc:creator,omitempty"`
-	Category       string        `xml:"category,omitempty"`
-	ContentEncoded *CDATA        `xml:"content:encoded,omitempty"`
+	Enclosure      *Enclosure    `xml:"enclosure,omitempty"`
 	MediaContent   *MediaContent `xml:"media:content,omitempty"`
+	ContentEncoded *CDATA        `xml:"content:encoded,omitempty"`
 }
 
 type Enclosure struct {
-	URL  string `xml:"url,attr"`
-	Type string `xml:"type,attr"`
+	URL    string `xml:"url,attr"`
+	Length string `xml:"length,attr,omitempty"` // Added length
+	Type   string `xml:"type,attr"`
 }
 
 type MediaContent struct {
@@ -75,19 +84,27 @@ func RSSHandler(dbClient *db.Client, siteURL string, siteName string) http.Handl
 		rss := RSS{
 			Version:      "2.0",
 			XmlnsAtom:    "http://www.w3.org/2005/Atom",
-			XmlnsMedia:   "http://search.yahoo.com/mrss/",
 			XmlnsDc:      "http://purl.org/dc/elements/1.1/",
+			XmlnsMedia:   "http://search.yahoo.com/mrss/",
 			XmlnsContent: "http://purl.org/rss/1.0/modules/content/",
 			Channel: Channel{
 				Title:         siteName,
 				Link:          siteURL,
 				Description:   "Latest news and updates from " + siteName,
-				Language:      "en-us",
-				LastBuildDate: time.Now().Format(time.RFC1123),
+				Language:      "en-us", // TOI uses en-gb, we can stick to en-us or make dynamic
+				Copyright:     fmt.Sprintf("Copyright (C) %d %s", time.Now().Year(), siteName),
+				Docs:          siteURL + "/rss",
+				LastBuildDate: time.Now().Format(time.RFC1123Z),
 				AtomLink: &AtomLink{
 					Href: siteURL + "/rss",
 					Rel:  "self",
 					Type: "application/rss+xml",
+				},
+				// Add a default logo/image for the channel
+				Image: &ChannelImage{
+					Title: siteName,
+					Link:  siteURL,
+					URL:   siteURL + "/static/gheadlineicon.png", // Ensure this path is correct
 				},
 				Item: []Item{},
 			},
@@ -99,26 +116,32 @@ func RSSHandler(dbClient *db.Client, siteURL string, siteName string) http.Handl
 				link = fmt.Sprintf("%s/article/%s", siteURL, a.ID)
 			}
 
+			// Sanitize or fallback for author
+			author := a.Author
+			if author == "" {
+				author = siteName
+			}
+
 			item := Item{
-				Title:       a.Title,
-				Link:        link,
-				Description: a.Excerpt,
-				PubDate:     a.CreatedAt.Format(time.RFC1123),
-				GUID:        link,
-				Creator:     a.Author,
-				Category:    a.Category,
+				Title:       CDATA{Value: a.Title},
+				Description: CDATA{Value: a.Excerpt},
+				Link:        CDATA{Value: link},
+				GUID:        CDATA{Value: link},
+				PubDate:     a.CreatedAt.Format(time.RFC1123Z),
+				Creator:     author,
 			}
 
 			if a.Content != "" {
 				item.ContentEncoded = &CDATA{Value: a.Content}
 			}
 
-			// Add image enclosure and media:content for better compatibility
+			// Add image enclosure and media:content
 			if a.ImageURL != "" {
-				// Standard Enclosure (Podcasts/Legacy)
+				// Standard Enclosure
 				item.Enclosure = &Enclosure{
-					URL:  a.ImageURL,
-					Type: "image/jpeg",
+					URL:    a.ImageURL,
+					Type:   "image/jpeg",
+					Length: "0", // Supabase doesn't easily give us size, using 0 or omitted is better than wrong. TOI has it.
 				}
 				// Media RSS (Google News/Discovery)
 				item.MediaContent = &MediaContent{
